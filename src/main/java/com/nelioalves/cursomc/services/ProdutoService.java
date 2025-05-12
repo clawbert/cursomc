cad79de41535c1389a694f1661a3979b336e9338
Clawbert Barbosa <clawbertpereira@gmail.com>
2025-05-12 11:35:56 -0400

    Produto e associação muitos para muitos

---
+++ src/main/java/com/nelioalves/cursomc/services/ProdutoService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.services;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto buscar(Integer id) {
+        Optional<Produto> obj = produtoRepository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
